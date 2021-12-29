7514cd2638007368722cee8268a9a5ef687cd932
Hugo Palomera <85078686+hugo-pb@users.noreply.github.com>
2021-12-29 10:40:27 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Search from "./Search";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hweather App</h1>
+      <Search />
+    </div>
+  );
+}
---
+++ src/Search.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+export default function Search() {
+  let [search, setSearch] = useState("");
+  let [result, setResult] = useState([]);
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c69faf9d2df6cf97f08634b08792a39d&units=metric`;
+  axios.get(url).then(showT);
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (search === "") return;
+  }
+  function showT(r) {
+    setResult(r.data.main.temp);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="looking for weather?"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <p>{result}</p>
+    </div>
+  );
+}
