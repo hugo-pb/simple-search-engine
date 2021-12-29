32c149d168a9460e0d48bb22a6ac74e776e69de3
Hugo Palomera <85078686+hugo-pb@users.noreply.github.com>
2021-12-29 10:50:00 -0700

    added message state

--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 export default function Search() {
   let [search, setSearch] = useState("");
-  let [result, setResult] = useState([]);
-
+  let [result, setResult] = useState("");
+  const [message, setMessage] = useState("");
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c69faf9d2df6cf97f08634b08792a39d&units=metric`;
   axios.get(url).then(showT);
   function handleSubmit(event) {
     event.preventDefault();
     if (search === "") return;
+    setMessage(
+      <p>
+        {" "}
+        the temperature in {search} is {result}{" "}
+      </p>
+    );
   }
   function showT(r) {
     setResult(r.data.main.temp);
@@ -25,7 +31,7 @@ export default function Search() {
         />
         <input type="submit" value="Search" />
       </form>
-      <p>{result}</p>
+      {message}
     </div>
   );
 }
