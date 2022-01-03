d69edcc072cd083126161a92f9ab2f035894ae85
Hugo Palomera <85078686+hugo-pb@users.noreply.github.com>
2022-01-02 21:05:40 -0700

    done

--- src/Search.js
+++ src/Search.js
@@ -2,22 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 export default function Search() {
   let [search, setSearch] = useState("");
-  let [result, setResult] = useState("");
-  const [message, setMessage] = useState("");
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c69faf9d2df6cf97f08634b08792a39d&units=metric`;
-  axios.get(url).then(showT);
+  let [message, setmessage] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    if (search === "") return;
-    setMessage(
-      <p>
-        {" "}
-        the temperature in {search} is {result}{" "}
-      </p>
-    );
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=ca5da085c3334fa2974d520a9a4b8c12&units=metric`;
+    axios.get(url).then(showT);
   }
   function showT(r) {
-    setResult(r.data.main.temp);
+    setmessage(
+      <p>
+        the Weather in {search} is {Math.round(r.data.main.temp)}{" "}
+      </p>
+    );
   }
 
   return (
@@ -26,7 +24,6 @@ export default function Search() {
         <input
           type="search"
           placeholder="looking for weather?"
-          value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
         <input type="submit" value="Search" />
